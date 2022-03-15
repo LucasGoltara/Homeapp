630108cdd6b144d21188380cbf16cc7837f4fba6
Lucas Goltara <goltara6@gmail.com>
2022-03-15 11:32:31 -0300

    qualquer coisa

--- lib/main.dart
+++ lib/main.dart
@@ -34,57 +34,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: Text(
-                      'Esqueceu sua senha?',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        height: 1,
-                      ),
-                    ),
-                  ),
-                ],
+            Center(
+              child: Text(
+                'Perfil',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: Text(
-                      'Preencha o campo abaixo e enviaremos um código de verificação de cadastro',
-                      style: TextStyle(fontSize: 19),
-                    ),
+            IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Deletar conta',
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange,
                   ),
-                  // RaisedButton(
-                  //     child: Container(
-                  //       width: 300,
-                  //       child: Padding(
-                  //         padding: const EdgeInsets.all(15.0),
-                  //         child: Text(
-                  //           'Resetar senha',
-                  //           style: TextStyle(color: Colors.white, fontSize: 20),
-                  //           textAlign: TextAlign.center,
-                  //         ),
-                  //       ),
-                  //     ),
-                  //     textColor: Colors.white,
-                  //     color: Colors.purple[800],
-                  //     onPressed: () {})
-                ],
-              ),
-            )
+                ))
           ],
         ),
       ),
     );
   }
 }
+
+
+// RaisedButton(
+//                       child: Container(
+//                         width: 300,
+//                         child: Padding(
+//                           padding: const EdgeInsets.all(15.0),
+//                           child: Text(
+//                             'Atualizar',
+//                             style: TextStyle(color: Colors.white, fontSize: 20),
+//                             textAlign: TextAlign.center,
+//                           ),
+//                         ),
+//                       ),
+//                       textColor: Colors.white,
+//                       color: Colors.purple[800],
+//                       onPressed: () {})
