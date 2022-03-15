1a8ddcff4b031583caa2f77cd7532db161693e4d
Lucas Goltara <goltara6@gmail.com>
2022-03-15 17:42:50 -0300

    15/03/2002

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  get emailcontroller => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,44 +36,211 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+                Text('Perfil',
+                    style:
+                        TextStyle(fontSize: 29, fontWeight: FontWeight.w400)),
+                SizedBox(
+                  width: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Deletar conta',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                      ),
+                    ))
+              ],
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 130, top: 1),
+                      child: Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple[800],
+                              width: 4,
+                            ),
+                            borderRadius: BorderRadius.circular(34),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: ClipRRect(
+                              child: Image.network(
+                                  'https://cdn.discordapp.com/attachments/936306543719223387/953334596387360950/fotografia-para-perfil.jpg'),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          height: 120,
+                          width: 120,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 130),
+                            child: Text(
+                              'Thiago',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.w600),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 100),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          '1',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Text('Seus Cursos')
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          '2',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Text('Ebooks')
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
               child: Text(
-                'Perfil',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                'Dados do perfil',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Deletar conta',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.orange,
-                  ),
-                ))
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                'Nome Completo',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                  icon: Icon(Icons.edit_outlined),
+                  onPressed: () {},
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                'E-mail de cadastro',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                  icon: Icon(Icons.edit_outlined),
+                  onPressed: () {},
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                'Celular / Whatsapp',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                  icon: Icon(Icons.edit_outlined),
+                  onPressed: () {},
+                )),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: RaisedButton(
+                    child: Container(
+                      width: 350,
+                      child: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Text(
+                          'Atualizar',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    textColor: Colors.white,
+                    color: Colors.purple[800],
+                    onPressed: () {}),
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
-// RaisedButton(
-//                       child: Container(
-//                         width: 300,
-//                         child: Padding(
-//                           padding: const EdgeInsets.all(15.0),
-//                           child: Text(
-//                             'Atualizar',
-//                             style: TextStyle(color: Colors.white, fontSize: 20),
-//                             textAlign: TextAlign.center,
-//                           ),
-//                         ),
-//                       ),
-//                       textColor: Colors.white,
-//                       color: Colors.purple[800],
-//                       onPressed: () {})
